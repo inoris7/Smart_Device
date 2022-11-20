f4096d7d84f3589999d66e1d7b5d3328adb6021d
inoris7 <nil.inoris@gmail.com>
2022-11-21 01:21:12 +0300

    Правки после тестирования

--- source/js/modules/modal-open-close.js
+++ source/js/modules/modal-open-close.js
@@ -2,6 +2,7 @@ const modalOpenButton = document.querySelector('.main-header__call-button');
 const modalWindow = document.querySelector('.main-modal__container');
 const modalCloseButton = document.querySelector('.main-modal__close-button');
 const nameField = document.querySelector('#modal-name-field');
+const body = document.querySelector('body');
 
 const isEscapeKey = (evt) => (evt.key === 'Escape');
 
@@ -11,16 +12,19 @@ if (modalOpenButton && modalWindow && modalCloseButton && nameField) {
     if (isEscapeKey(evt)) {
       evt.preventDefault();
       modalWindow.classList.add('main-modal__container--closed');
+      body.classList.remove('main-modal--open');
     }
   };
 
   modalOpenButton.addEventListener('click', () => {
     modalWindow.classList.remove('main-modal__container--closed');
     nameField.focus();
+    body.classList.add('main-modal--open');
   });
 
   modalCloseButton.addEventListener('click', () => {
     modalWindow.classList.add('main-modal__container--closed');
+    body.classList.remove('main-modal--open');
   });
 
   document.addEventListener('keydown', onModalEscButtonKeydown);
--- source/js/modules/phone-mask.js
+++ source/js/modules/phone-mask.js
@@ -3,7 +3,7 @@ import Inputmask from 'inputmask';
 const phoneFields = document.querySelectorAll('input[type="tel"]');
 
 if (phoneFields) {
-  const im = new Inputmask({mask: '+7(999)-999-99-99', jitMasking: true});
+  const im = new Inputmask('+7(999)-999-99-99');
 
   phoneFields.forEach((el) => {
     im.mask(el);
