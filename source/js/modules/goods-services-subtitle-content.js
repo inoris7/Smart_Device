fd56febeae8e3f8f10daa7f6285d879aced21dda
inoris7 <nil.inoris@gmail.com>
2022-11-22 17:17:53 +0300

    исправления после второго теста

--- source/js/modules/goods-services-subtitle-content.js
+++ source/js/modules/goods-services-subtitle-content.js
@@ -1,11 +1,17 @@
 const subtitle = document.querySelector('.goods-services__subtitle');
+const cardSubtitle = document.querySelector('.goods-services__card-subtitle--second');
 
-if (subtitle) {
+if (subtitle && cardSubtitle) {
   const checkMediaQuery = () => {
     if (window.innerWidth >= 768) {
       subtitle.innerHTML = 'Smart Device предлагает следующие товары и услуги';
     } else {
       subtitle.innerHTML = 'Товары и услуги<br> Smart Device';
+    }
+    if (window.innerWidth >= 1024) {
+      cardSubtitle.innerHTML = 'Монтаж печатных<br> плат';
+    } else {
+      cardSubtitle.innerHTML = 'Монтаж<br> печатных плат';
     }
   };
 
---
+++ source/js/modules/modal-focus-trap.js
@@ -0,0 +1,4 @@
+import * as focusTrap from 'focus-trap';
+
+const trap = focusTrap.createFocusTrap('.main-modal__field');
+trap.activate();
